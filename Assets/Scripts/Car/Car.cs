25b25f6765bfdc9a91647af796978104ac6149f1
Logan Gilmour <logan@scopear.com>
2015-07-19 16:30:30 -0600

    Add car movement beginnings

---
+++ Assets/Scripts/Car/Car.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+	float CarAngle = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localRotation = Quaternion.AngleAxis(Input.GetAxis("Horizontal")*(Input.GetButton("Boost")?10f:(Mathf.Lerp(30f,90f,Input.GetAxis("Break")))),Vector3.up);
+	}
+}
