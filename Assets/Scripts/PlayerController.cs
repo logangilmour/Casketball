bdaf71c7756b26060020f1cd8f804896caa7d07a
Sohail Rajabi <sohail@scopear.com>
2015-07-18 20:00:50 -0600

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float maxVelocity = 10.0f;
+
+	private float velocity = 0.0f;
+	private float distance = 0.0f;
+	
+	private TrackManager tm;
+	
+	// Use this for initialization
+	void Start () {
+		tm = FindObjectOfType(typeof(TrackManager)) as TrackManager;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Vertical");
+		//Debug.Log (h);
+		if(Input.GetKey("up"))
+		{
+			velocity = (Time.deltaTime + velocity) % velocity;
+			Debug.Log (velocity);
+		}
+		
+		distance += Time.deltaTime * velocity;
+		
+		SplineInterpolator.TransformRotation tr = tm.si.GetTransformRotationAtLen(distance);
+		
+		if(tr == null)
+		{
+			distance = 0;
+		}
+		else
+		{
+			this.gameObject.transform.position = tr.pos;
+			this.gameObject.transform.rotation = tr.rot;
+		}
+	}
+}
---
+++ Assets/Scripts/TrackManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackManager : MonoBehaviour {
+	public static TrackManager instance;
+	
+	public SplineController sc;
+	public SplineInterpolator si;
+	
+	void Start()
+	{
+		if(sc == null)
+		{
+			sc = this.GetComponent<SplineController>() as SplineController;
+		}
+		
+		if(si == null)
+		{
+			si = this.GetComponent<SplineInterpolator>() as SplineInterpolator;
+		}
+	}
+}
